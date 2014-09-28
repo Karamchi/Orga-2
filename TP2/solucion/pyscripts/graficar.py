6af02860d48427037047081c675aacecc7516f13
nicoroulet <nicoroulet@gmail.com>
2014-09-28 17:10:04 -0300

    pyscripts arreglados

--- TP2/solucion/pyscripts/graficar.py
+++ TP2/solucion/pyscripts/graficar.py
@@ -8,16 +8,27 @@ import decimal
 
 datos=[]
 for f in argv[1:]:
-	datos.append([s[:s.find(' ')] for s in open(f,'r').readlines()])
+	datos.append([int(s[:s.find(' ')]) for s in open(f,'r').readlines()])
 
 #files=[[] for arg in argv[1:]]
-plt.figure(num=1)#, figsize=(12,5))
-for i in range(len(datos)):
+#plt.figure(num=1, figsize=(12,5))
+labels=[a.split('/')[1].split('.')[0] for a in argv[1:]]
+#plt.boxplot(datos, labels=labels, meanline=True)
+# demonstrate how to toggle the display of different elements:
+fig=plt.figure(1)
+ax = fig.add_subplot(111)#, figsize=(12,6))
+ax.boxplot(datos)
+ax.set_xticklabels(labels)
+ax.set_title(labels[0].split('_')[0])
+
+
+'''
 	plt.subplot(1, len(datos), i)
 	plt.plot(datos[i], marker='o')
-	plt.axhline( np.mean(datos[i]), color='b', linestyle='dashed', linewidth=3)
+#	plt.axhline( np.mean(datos[i]), color='b', linestyle='dashed', linewidth=3)
 	plt.title(argv[i+1].split('.')[0])
+'''
 
-plt.savefig(argv[1].split('_')[0]+'_time.png',dpi=100)
+plt.savefig(argv[1].split('.')[0]+'.png',dpi=100)#argv[1].split('_')[0]+'_time.png',dpi=100)
 
 
