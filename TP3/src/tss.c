766c9d1f9b1694a67694428387d155a07b6fb780
Javier Nicolas Minces Muller <jminces@ws21.labo6>
2014-11-04 21:49:19 -0300

    TSS

--- TP3/src/tss.c
+++ TP3/src/tss.c
@@ -14,13 +14,12 @@ tss tss_idle;
 tss tss_zombisA[CANT_ZOMBIS];
 tss tss_zombisB[CANT_ZOMBIS];
 
-void tss_completar_libre(tss t, char tipo, char jugador, int pos/*, bool interr*/){
-		breakpoint();
+void tss_completar_libre(tss t, char tipo, char jugador, int pos){
 		int cr3 = mmu_inicializar_dir_zombi(tipo, jugador, pos);
 		t.cr3 = cr3;
 		jugador -= 65; // jugador A: 0, jugador B: 1 
 		t.eip = pos_mapa(1+jugador*75, pos);
-		t.eflags=0x02/*+interr*(0x200)*/; //VER Q ONDA DSD EL ASM COMO VER SI ESTÁ HABILITADO LAS INTERRUP
+		t.eflags=0x202
 		t.esp = t.eip; //CONFUSOOO!! PREGUNTAR (Tiene q ser la base de la tarea o pedir una nueva pagina?)
 		t.ebp = t.eip; //CONFUSOOO!! PREGUNTAR (Tiene q ser la base de la tarea o pedir una nueva pagina?)
 }
@@ -29,46 +28,19 @@ void tss_inicializar() {
 	gdt[GDT_TAREA_INICIAL].base_0_15 = (int)&tss_inicial & 0xFFFF;
 	gdt[GDT_TAREA_INICIAL].base_23_16 = ((int)&tss_inicial >> 16) & 0xFF;
 	gdt[GDT_TAREA_INICIAL].base_31_24 = (int)&tss_inicial >> 24;
-	/*tss_inicial = (tss) {
-		(unsigned short)  0x00,	//ptl;
-		(unsigned short)  0x00,	//unused0;
-		(unsigned int)    0x00,	//esp0;
-		(unsigned short)  0x00,	//ss0;
-		(unsigned short)  0x00,	//unused1;
-		(unsigned int)    0x00,	//esp1;
-		(unsigned short)  0x00,	//ss1;
-		(unsigned short)  0x00,	//unused2;
-		(unsigned int)    0x00,	//esp2;
-		(unsigned short)  0x00,	//ss2;
-		(unsigned short)  0x00,	//unused3;
-		(unsigned int)    0x00,	//cr3;
-		(unsigned int)    0x00,	//eip;
-		(unsigned int)    0x00,	//eflags;
-		(unsigned int)    0x00,	//eax;
-		(unsigned int)    0x00,	//ecx;
-		(unsigned int)    0x00,	//edx;
-		(unsigned int)    0x00,	//ebx;
-		(unsigned int)    0x00,	//esp;
-		(unsigned int)    0x00,	//ebp;
-		(unsigned int)    0x00,	//esi;
-		(unsigned int)    0x00,	//edi;
-		(unsigned short)  0x00,	//es;
-		(unsigned short)  0x00,	//unused4;
-		(unsigned short)  0x00,	//cs;
-		(unsigned short)  0x00,	//unused5;
-		(unsigned short)  0x00,	//ss;
-		(unsigned short)  0x00,	//unused6;
-		(unsigned short)  0x00,	//ds;
-		(unsigned short)  0x00,	//unused7;
-		(unsigned short)  0x00,	//fs;
-		(unsigned short)  0x00,	//unused8;
-		(unsigned short)  0x00,	//gs;
-		(unsigned short)  0x00,	//unused9;
-		(unsigned short)  0x00,	//ldt;
-		(unsigned short)  0x00,	//unused10;
-		(unsigned short)  0x00,	//dtrap;
-		(unsigned short)  0x00,	//iomap;
-	};*/
+
+	int i;	
+	for (i=0;i<CANT_ZOMBIS;i++) {
+		gdt[GDT_ZOMBIE_A8+i].base_0_15 = (int)&(tss_zombisA[i]) & 0xFFFF;
+		gdt[GDT_ZOMBIE_A8+i].base_23_16 = ((int)&(tss_zombisA[i]) >> 16) & 0xFF;
+		gdt[GDT_ZOMBIE_A8+i].base_31_24 = (int)&(tss_zombisA[i]) >> 24;
+	}
+	
+	for (i=0;i<CANT_ZOMBIS;i++) {
+		gdt[GDT_ZOMBIE_B8+i].base_0_15 = (int)&(tss_zombisB[i]) & 0xFFFF;
+		gdt[GDT_ZOMBIE_B8+i].base_23_16 = ((int)&(tss_zombisB[i]) >> 16) & 0xFF;
+		gdt[GDT_ZOMBIE_B8+i].base_31_24 = (int)&(tss_zombisB[i]) >> 24;
+	}
 }
 
 void tss_inicializar_idle() {
